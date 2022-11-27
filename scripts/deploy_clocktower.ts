1ad8c88960fb72a0b862592cb46df0b053df62c8
Hugo Marx <vhmarx@haderachllc.com>
2022-11-27 07:01:52 -0600

    updated deployscript and ran it

--- scripts/deploy_clocktower.ts
+++ scripts/deploy_clocktower.ts
@@ -2,12 +2,20 @@ import { ethers } from "hardhat";
 
 async function main() {
 
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
     const Clocktower = await ethers.getContractFactory("Clocktower");
     const clocktower = await Clocktower.deploy();
 
     await clocktower.deployed();
 
     console.log("Clocktower deployed...");
+
+    console.log("Contract address:", clocktower.address);
 
 }
 
