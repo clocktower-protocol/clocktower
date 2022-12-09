cedc75309e9d5c2d1c864de9d002dbc7b13c5d15
Hugo Marx <hugo@clocktower.finance>
2022-12-09 05:01:56 -0600

    fixed bug in sendTransaction. Now stamps send

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-abi-exporter";
 
 const config: HardhatUserConfig = {
+  
+  networks: {
+    hardhat: {
+      gasPrice: 20000000000
+    }
+  },
   solidity: "0.8.17",
+  
 };
 
 
