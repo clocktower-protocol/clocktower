aa5bbd79480632324ea6f65eea351a1fa01f0eb3
Hugo Marx <hugo@clocktower.finance>
2022-12-31 13:55:31 -0600

    brought back cancelTransaction so eth could be returned. Made remove a private function

--- test/Clocktower.ts
+++ test/Clocktower.ts
@@ -211,7 +211,7 @@ describe("Clocktower", function(){
             const {hardhatClocktower, owner, otherAccount} = await loadFixture(deployClocktowerFixture);
             //await hardhatClocktower.addTransaction(otherAccount.address, hourAhead, eth, testParams)
             let transactions: any = await hardhatClocktower.getAccountTransactions();
-            await hardhatClocktower.removeTransaction(transactions[0].id, transactions[0].timeTrigger)
+            await hardhatClocktower.cancelTransaction(transactions[0].id, transactions[0].timeTrigger, transactions[0].token)
             let transactions2: any = await hardhatClocktower.getAccountTransactions();
             expect(
                 transactions2.length
