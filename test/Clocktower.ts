d22399edf5f4c997c82db2fc2f60bf7242f16e44
Hugo Marx <hugo@clocktower.finance>
2023-01-04 15:52:41 -0600

    updated function name

--- test/Clocktower.ts
+++ test/Clocktower.ts
@@ -235,7 +235,7 @@ describe("Clocktower", function(){
         })
         it("Should find day of month", async function() {
             const {hardhatClocktower, owner, otherAccount} = await loadFixture(deployClocktowerFixture);
-            expect(await hardhatClocktower.unixTimeToDayOfMonth(1739340000))
+            expect(await hardhatClocktower.unixTimeToDayMonthYear(1739340000))
 
         })
         
