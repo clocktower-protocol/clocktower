93e51db233a245420a64200004bd1a20be68a3a2
Hugo Marx <hugo@clocktower.finance>
2023-01-26 10:39:47 -0600

    updated and cleaned up deploy script

--- scripts/deploy_clocktower.ts
+++ scripts/deploy_clocktower.ts
@@ -9,32 +9,19 @@ async function main() {
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const TestUser = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
-    const Clocktower = await ethers.getContractFactory("Clocktower");
+    
     const ClockToken = await ethers.getContractFactory("CLOCKToken");
-    const ClockSubscribe = await ethers.getContractFactory("contracts/ClocktowerSubscribe.sol")
-    const ClockPayment = await ethers.getContractFactory("contracts/ClockTowerPayment.sol")
+    const ClockSubscribe = await ethers.getContractFactory("ClockTowerSubscribe")
+    const ClockPayment = await ethers.getContractFactory("ClockTowerPayment")
 
-    //const ClockPure = await ethers.getContractFactory("contracts/ClocktowerPure.sol");
-    //const ClockLibrary = await ethers.getContractFactory("ClockTowerLibrary");
-
-    const clocktower = await Clocktower.deploy();
     const clockSubscribe = await ClockSubscribe.deploy();
     const clockPayment = await ClockPayment.deploy();
     const clockToken = await ClockToken.deploy(ethers.utils.parseEther("100000"));
-    //const clockPure = await ClockPure.deploy();
-    
-    //const clockLibrary = await ClockLibrary.deploy();
-
-    await clocktower.deployed();
+  
     await clockSubscribe.deployed();
     await clockPayment.deployed();
-   // await clockPure.deployed();
 
     console.log("Clocktower deployed...");
-    
-
-    console.log("Contract address:", clocktower.address);
-   // console.log("ClockPure address", clockPure.address);
 
     await clockToken.deployed();
 
@@ -43,7 +30,6 @@ async function main() {
     console.log("Contract address:", clockToken.address);
 
     //approve token for clocktower
-    await clocktower.addERC20Contract(clockToken.address);
     await clockPayment.addERC20Contract(clockToken.address);
     console.log("Approved contract..."+clockToken.address);
 
@@ -55,9 +41,8 @@ async function main() {
     console.log("ClocktowerSubscribe Deployed!")
     console.log("Contract address:", clockSubscribe.address);
 
-
-   // await clockLibrary.deployed();
-    //console.log("Contract address:", clockLibrary.address)
+    console.log("ClocktowerPayment Deployed!")
+    console.log("Contract address:", clockPayment.address);
 }
 
 main().catch((error) => {
