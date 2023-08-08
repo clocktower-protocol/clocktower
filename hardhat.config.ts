d29288a218118882499d117feaf11255ef9c46f3
Hugo Marx <vhmarx@haderachllc.com>
2023-08-08 11:29:38 -0500

    updated npm libraries

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import "hardhat-contract-sizer";
 const config: HardhatUserConfig = {
   
   solidity: {
-    version: "0.8.9",
+    version: "0.8.21",
     settings: {
       optimizer: {
         enabled: true,
