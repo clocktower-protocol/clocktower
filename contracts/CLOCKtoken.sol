f24404aea0b613d38ed26ed8931ae665f5ed61a8
Hugo Marx <hugo@clocktower.finance>
2024-03-01 14:13:53 -0600

    updated clock token to make it erc20

--- contracts/CLOCKtoken.sol
+++ contracts/CLOCKtoken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.24;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract CLOCKToken is ERC20Permit {
+contract CLOCKToken is ERC20{
 
-   constructor(uint256 initialSupply) ERC20Permit("CLockTower") ERC20("CLockTower", "CLOCK") {
+   constructor(uint256 initialSupply) ERC20("CLockTower", "CLOCK") {
         _mint(msg.sender, initialSupply);
    }
 
