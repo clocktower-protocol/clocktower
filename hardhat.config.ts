8c76b54894965384c0c76e88c7f3c3821d81abbe
Hugo Marx <hugo@clocktower.finance>
2024-07-22 15:41:41 -0500

    added gas reporter config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,6 +18,10 @@ const config: HardhatUserConfig = {
     hardhat: {
       gasPrice: 30000000000
     }
+  },
+  gasReporter: {
+    enabled: true,
+    coinmarketcap: "14c853e7-3fe7-4254-8863-81d85f79a253"
   }
   
 };
