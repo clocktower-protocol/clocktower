9c402ceb3f0da9a6c8d42e7a90193a813d98f42d
vhmarx <hugo@clocktower.finance>
2025-10-03 13:59:17 -0500

    added moch tookbox lib

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import type { HardhatUserConfig } from "hardhat/config";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 
 const config: HardhatUserConfig = {
+    plugins: [hardhatToolboxMochaEthers],
     solidity: {
         compilers: [
         {
