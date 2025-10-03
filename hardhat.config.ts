612d1df2d69cf314b25502ecb0bf5ec16a68b13c
vhmarx <hugo@clocktower.finance>
2025-10-03 13:56:34 -0500

    hardhat build success

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,20 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
-const config: HardhatUserConfig = {};
+const config: HardhatUserConfig = {
+    solidity: {
+        compilers: [
+        {
+          version: "0.8.28",
+          settings: {
+            //viaIR: true,
+            optimizer: {
+              enabled: true,
+              runs: 500,
+            },
+            evmVersion: "cancun"
+          },
+        }]
+      },
+};
 
 export default config;
